6397fc40dd7aa485f18baa0985724bfc5ba71b64
Odom Ear <odom.ear@hotmail.com>
2019-02-14 12:19:13 +0100

    commit

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 /*
 Imports
 */
+    //=> Use.env
+    require('dotenv').config();
+    const express = require('express'); //Gestion du serveur
+    const path = require('path'); //Gestion du dossier client
+    const bodyParser = require ('body-parser'); //recupére donnée dans requetes
+    const ejs = require('ejs'); //Gestion du moteur de rendu
 //
 
 /*
 Configuration
 */
+    const server = express();
+    const port = process.env.PORT;
 //
 
 /*
 Démarrer le serveur
 */
+    server.listen(port, ()=> {
+        console.log(`Server is active on port ${port}`);
+    });
 //
